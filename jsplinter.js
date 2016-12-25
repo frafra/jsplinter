3d279a17ab9e84578983d15dfee58ec314ce5b8b
Francesco Frassinelli <fraph24@gmail.com>
2016-12-25 13:28:55 +0100

    Design matrix for 1st order splines

--- jsplinter.js
+++ jsplinter.js
@@ -1,12 +1,45 @@
-var input = new Matrix({
-  target: document.querySelector('main'),
+var observationsMatrix = new Matrix({
+  target: document.querySelector('observationsMatrix'),
   data: {
     headings: ['x', 'y'],
-    matrix: math.matrix([[0, 1], [1, 3], [2, 2]]),
+    matrix: math.matrix([[-0.1, 1], [1, 3], [2.5, 2]]),
     editable: true,
     tmp: [{value:null, validity:false}, {value:null, validity:false}]
   },
 });
+
+
+var designMatrix = new Matrix({
+  target: document.querySelector('designMatrix'),
+  data: {
+    headings: [],
+    matrix: math.matrix([]),
+    editable: false
+  },
+});
+
+function interpolation() {
+  var matrix = observationsMatrix.get('matrix');
+  var xVector = math.multiply(matrix, [1, 0]);
+  var xMin = math.floor(math.min(xVector));
+  var xMax = math.ceil(math.max(xVector));
+  var aIndex = math.range(xMin, xMax, 1, true);
+  var a = math.zeros(matrix.size()[0], aIndex.size()[0]);
+  xVector.toArray().forEach((item, index) => {
+    a.set([index, math.floor(item)-xMin], 1-math.abs(item%1));
+    if (item%1) {
+      a.set([index, math.floor(item)+1-xMin], math.abs(item%1));
+    }
+  });
+  designMatrix.set({
+    headings: aIndex.toArray(),
+    matrix: a,
+    editable: false,
+  });
+  /* To be continued... */
+}
+
+observationsMatrix.observe('matrix', interpolation);
 
 var chart = new Chart('graph', {
   type: 'line',
@@ -31,11 +64,11 @@ function arrayToMap(item) {
 }
 
 function draw() {
-  var array = input.get('matrix').toArray();
+  var array = observationsMatrix.get('matrix').toArray();
   array.sort(function (a, b) {return a[0]-b[0]});
   chart.data.datasets[0].data = array.map(arrayToMap);
   chart.update();
 }
 
-input.observe('matrix', draw);
+observationsMatrix.observe('matrix', draw);
 
